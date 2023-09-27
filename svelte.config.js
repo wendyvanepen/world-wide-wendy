9e1c669e85f749269a55c08d830fae5ea2edde6f
Wendy van Epen <wendyvanepen@MacBook-Pro-2.local>
2023-09-27 09:47:42 +0200

    fix: fix for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		// Github pages serves from a subdirectory, so we need to set paths.base
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/world-wide-wendy' : ''
+		}
 	},
 	preprocess: vitePreprocess()
 };
