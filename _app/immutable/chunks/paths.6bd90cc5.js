8c465a5a026e2ea364e683ff8f68a81e569ad1d5
Wendy van Epen <wendyvanepen@MacBook-Pro-2.local>
2023-09-27 09:56:48 +0200

    Updates

---
+++ _app/immutable/chunks/paths.6bd90cc5.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1ti0jw5)==null?void 0:s.base)??"/world-wide-wendy";var e;const a=((e=globalThis.__sveltekit_1ti0jw5)==null?void 0:e.assets)??t;export{a,t as b};
